083e37d8131f60f7e5c54381e4a695d398d8f336
surevirus970-sudo <surevirus970@mail.ru>
2025-11-12 14:07:33 +0300

    Update CODE_121125.cpp

--- CODE_121125.cpp
+++ CODE_121125.cpp
@@ -1,36 +1,61 @@
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-double fractionalKnapsack(vector<pair<int, int>> items, int capacity) {
-    vector<pair<double, pair<int, int>>> sorted;
-    for (auto [w, v] : items)
-        sorted.push_back({(double)v / w, {w, v}});
-    sort(sorted.begin(), sorted.end(), greater<>());
+struct Item {
+    int weight;
+    int value;
+    double ratio;
+};
 
-    double totalValue = 0;
-    int used = 0;
-
-    for (auto [ratio, item] : sorted) {
-        int w = item.first, v = item.second;
-        if (used + w <= capacity) {
-            totalValue += v;
-            used += w;
-        } else {
-            int left = capacity - used;
-            totalValue += ratio * left;
-            break;
-        }
-    }
-    return totalValue;
+// Функция сравнения для сортировки по убыванию удельной стоимости
+bool compareItems(const Item& a, const Item& b) {
+    return a.ratio > b.ratio;
 }
 
 int main() {
-    vector<pair<int, int>> items = {{2,10}, {3,20}, {4,15}, {5,25}};
-    int capacity = 8;
-    cout << "Максимальная стоимость: " << fractionalKnapsack(items, capacity) << endl;
+    int n;
+    cout << "Введите количество предметов: ";
+    cin >> n;
+
+    vector<Item> items(n);
+    cout << "Введите вес и стоимость каждого предмета (по паре чисел на строку):\n";
+    for (int i = 0; i < n; ++i) {
+        cin >> items[i].weight >> items[i].value;
+        items[i].ratio = (double)items[i].value / items[i].weight;
+    }
+
+    int capacity;
+    cout << "Введите вместимость рюкзака: ";
+    cin >> capacity;
+
+    // Сортируем по убыванию удельной стоимости
+    sort(items.begin(), items.end(), compareItems);
+
+    double totalValue = 0.0;
+    int used = 0;
+
+    cout << "\nВыбор предметов:\n";
+    for (int i = 0; i < n; ++i) {
+        if (used + items[i].weight <= capacity) {
+            totalValue += items[i].value;
+            used += items[i].weight;
+            cout << "Взят предмет целиком: вес=" << items[i].weight
+                 << ", стоимость=" << items[i].value << endl;
+        } else {
+            int remaining = capacity - used;
+            if (remaining > 0) {
+                double partValue = items[i].ratio * remaining;
+                totalValue += partValue;
+                cout << "Взята часть предмета: " << remaining << " из "
+                     << items[i].weight << " (стоимость=" << partValue << ")" << endl;
+            }
+            break;
+        }
+    }
+
+    cout << "\nМаксимальная стоимость: " << totalValue << endl;
     return 0;
 }
